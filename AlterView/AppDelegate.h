70f8d3aa167d23dcadb8f59a7c2bcaec2ef52999
MOYO <498389182@qq.com>
2019-07-19 09:35:23 +0800

    新建

---
+++ AlterView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AlterView
+//
+//  Created by MOYO on 2019/7/19.
+//  Copyright © 2019年 MOYO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
